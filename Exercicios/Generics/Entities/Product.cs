e4b507093e6f6009d12b80923bbe746dddb67593
Lucas da Cunha <luques.cunha@gmail.com>
2024-02-24 17:37:51 -0300

    generics e comparable

---
+++ Exercicios/Generics/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Entities
+{
+    class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (!(obj is Product))
+            {
+                throw new ArgumentException("não é um produto");
+            }
+            Product other = (Product)obj;
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
--- Exercicios/Generics/PrintService.cs
+++ Exercicios/Generics/PrintService.cs
@@ -1,11 +1,11 @@
 ﻿namespace Generics
 {
-    class PrintService
+    class PrintService<T>
     {
-        private int[] _values = new int[10];
+        private T[] _values = new T[10];
         private int _count = 0;
 
-        public void AddValue(int value)
+        public void AddValue(T value)
         {
             if (_count == 10)
             {
@@ -15,9 +15,23 @@
             _count++;
         }
 
-        public int First()
+        public T First()
         {
             return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
         }
     }
 }
