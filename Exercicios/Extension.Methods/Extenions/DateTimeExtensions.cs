f3ae3c0f3d22ce0a2a36c1774c15590a07187d66
Lucas da Cunha <luques.cunha@gmail.com>
2024-02-25 23:17:19 -0300

    exetensions methods

---
+++ Exercicios/Extension.Methods/Extenions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace System
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now - thisObj;
+
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1") + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1") + " days";
+            }
+        }
+    }
+}
---
+++ Exercicios/Extension.Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2024, 2, 25, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Testando extensão de método para string.";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+
+}
