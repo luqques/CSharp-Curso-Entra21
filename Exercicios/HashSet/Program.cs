0f7aa2d858e0dedb055ed8785be637196aba8353
Lucas da Cunha <luques.cunha@gmail.com>
2024-02-25 22:20:39 -0300

    operações com HashSet e SortedSet

--- Exercicios/HashSet/Program.cs
+++ Exercicios/HashSet/Program.cs
@@ -5,6 +5,44 @@
         static void Main(string[] args)
         {
             HashSet<string> set = new HashSet<string>();
+
+            set.Add("Tv");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+
+            SortedSet<int> a = new SortedSet<int> { 0, 1, 2, 3 };
+            SortedSet<int> b = new SortedSet<int>() { 1, 3, 4, 5, 6 };
+
+            // Union
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
+
+            // Intersection
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+            // Difference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
