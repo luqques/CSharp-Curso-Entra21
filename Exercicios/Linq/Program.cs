b26e43dc841c0bbd51ab59b627e57a5223f0e882
Lucas da Cunha <luques.cunha@gmail.com>
2024-03-06 19:59:27 -0300

    introdução ao Linq

---
+++ Exercicios/Linq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
