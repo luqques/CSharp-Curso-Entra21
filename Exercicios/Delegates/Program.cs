4f38bb84f0792ff99678edc7c5bfcebb0606af04
Lucas da Cunha <luques.cunha@gmail.com>
2024-03-05 22:13:11 -0300

    delegates

---
+++ Exercicios/Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using Delegates.Services;
+
+namespace Exercicio
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+
+}
